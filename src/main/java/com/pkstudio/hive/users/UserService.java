9766abdec864fc46fb91df8f5d5f0e0f17f954fb
pky <pawel.kutesko@blstream.com>
2015-03-24 16:54:15 +0100

    userService security filter fix

--- src/main/java/com/pkstudio/hive/users/UserService.java
+++ src/main/java/com/pkstudio/hive/users/UserService.java
@@ -1,11 +1,11 @@
 package com.pkstudio.hive.users;
 
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PostAuthorize;
 
 public interface UserService {
 	
 	@Secured("ROLE_PLAYER")
-	@PostFilter("returnObject.username == principal")
+	@PostAuthorize("returnObject.username == principal")
 	public User getById(int id);
 }
